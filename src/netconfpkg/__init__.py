60c7634f5eebd80468e3f877b4fe6080d24f94b4
pknirsch <pknirsch>
2001-06-06 12:30:29 +0000

    - Nice little Python plugin package using automatic module import (dirty hack)

---
+++ src/netconfpkg/__init__.py
@@ -0,0 +1,30 @@
+## netconf - A network configuration tool
+## Copyright (C) 2001 Red Hat, Inc.
+## Copyright (C) 2001 Phil Knirsch <pknirsch@redhat.com>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+# Import all subpackages of our netconfpkg directory. This code is a real
+# dirty hack but does the job(tm). It basically finds all .py files in the
+# package directory and imports from all found files (except __init__.py that
+# is) ;). Nice for plugin mechanism.
+
+import os
+files = map(lambda v: v[:-3], filter(lambda v: v[-3:] == ".py" and v != "__init__.py", os.listdir(__path__[0])))
+
+for i in files:
+    cmd = "from " + i + " import *"
+    exec cmd
