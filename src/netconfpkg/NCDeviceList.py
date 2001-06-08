c57925ad74a9b70f3f31aa3cec1129b6074ddde5
harald <harald>
2001-06-08 11:25:35 +0000

    *** empty log message ***

---
+++ src/netconfpkg/NCDeviceList.py
@@ -0,0 +1,12 @@
+import DeviceList
+from DeviceList import *
+
+class DeviceList(DeviceList_base):
+        def __init__(self, list = None, parent = None):
+		DeviceList_base.__init__(self, list, parent)
+
+	def load(self):
+		pass
+
+	def save(self):
+		pass
