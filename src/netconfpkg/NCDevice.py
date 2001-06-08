59e09337f99df90155e5d31104fbe48c231df944
harald <harald>
2001-06-08 14:51:56 +0000

    initial version

---
+++ src/netconfpkg/NCDevice.py
@@ -0,0 +1,69 @@
+from DeviceList import *
+from NC_functions import *
+
+if not "/usr/lib/rhs/python" in sys.path:
+    sys.path.append("/usr/lib/rhs/python")
+import Conf
+
+class ConfDevice(Conf.ConfShellVar):
+    def __init__(self, name):
+        Conf.ConfShellVar.__init__(self, SYSCONFDEVICEDIR + name)  
+
+class Device(Device_base):
+    keydict = { 'DeviceId' : 'NAME',
+                'DeviceName' : 'DEVICE',
+                'OnBoot' : 'ONBOOT',
+                'IP' : 'IPADDR',
+                'Netmask' : 'NETMASK',
+                'DefaultGW' : 'GATEWAY',
+                'Hostname' : 'HOSTNAME',
+                'Domain' : 'DOMAIN',
+                'BootProto' : 'BOOTPROTO',
+                }
+
+    boolkeydict = { 'OnBoot' : 'ONBOOT',
+                    'AllowUser' : 'ALLOWUSER',
+                    'AutoDNS' : 'AUTODNS',
+                    }
+        
+    def __init__(self, list = None, parent = None):
+        Device_base.__init__(self, list, parent)        
+        conf = None
+
+    def load(self, name):        
+        conf = ConfDevice(name)
+        
+        for selfkey in self.keydict.keys():
+            confkey = self.keydict[selfkey]
+            if conf.has_key(confkey):
+                self.__dict__[selfkey] = conf[confkey]
+
+        for selfkey in self.boolkeydict.keys():
+            confkey = self.boolkeydict[selfkey]
+            if conf.has_key(confkey):
+                if conf[confkey] == 'yes':
+                    self.__dict__[selfkey] = true
+                else: self.__dict__[selfkey] = false
+            else: self.__dict__[selfkey] = false
+
+        if not self.DeviceId:
+            self.DeviceId = name
+            
+    def save(self):
+        conf = ConfDevice(self.DeviceId)
+
+        for selfkey in self.keydict.keys():
+            confkey = self.keydict[selfkey]
+            if self.__dict__[selfkey]:
+                conf[confkey] = "'" + str(self.__dict__[selfkey]) + "'"
+            else: conf[confkey] = "''"
+
+        for selfkey in self.boolkeydict.keys():
+            confkey = self.boolkeydict[selfkey]
+            if self.__dict__[selfkey]:
+                conf[confkey] = 'yes'
+            else:
+                conf[confkey] = 'no'
+
+        conf.write()
+                    
