a37bf99a99cc82a75a56ef88bc1efa04ab7b15b1
pknirsch <pknirsch>
2001-06-11 16:01:53 +0000

    - Added SYSCONFPROFILEDIR - Added generic_error_dialog() for later global use

--- src/netconfpkg/NC_functions.py
+++ src/netconfpkg/NC_functions.py
@@ -1,2 +1,21 @@
+import gnome
+import gnome.ui
+
 OLDSYSCONFDEVICEDIR='/etc/sysconfig/network-scripts/'
 SYSCONFDEVICEDIR='/etc/sysconfig/networking/devices/'
+SYSCONFPROFILEDIR='/etc/sysconfig/networking/profiles/'
+
+def generic_error_dialog (message, parent_dialog, dialog_type="warning", widget=None, page=0, broken_widget=None):
+    dialog = gnome.ui.GnomeMessageBox (message, dialog_type, "Button_Ok")
+    dialog.set_parent (parent_dialog)
+    if widget != None:
+        if isinstance (widget, gtk.GtkCList):
+            widget.select_row (page, 0)
+        elif isinstance (widget, gtk.GtkNotebook):
+            widget.set_page (page)
+    if broken_widget != None:
+        broken_widget.grab_focus ()
+        if isinstance (broken_widget, gtk.GtkEntry):
+            broken_widget.select_region (0, -1)
+    dialog.run ()
+
