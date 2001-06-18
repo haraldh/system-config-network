e0e7f30cc76a19c0efe9e1e7bae2940a2ea80863
pknirsch <pknirsch>
2001-06-18 08:55:42 +0000

    - Fixed DNS server reading.

--- src/netconfpkg/NCProfileList.py
+++ src/netconfpkg/NCProfileList.py
@@ -45,17 +45,15 @@ class ProfileList(ProfileList_base):
                 prof.HostsList.append(host)
             dnsconf.filename = SYSCONFPROFILEDIR + '/' + pr + '/resolv.conf'
             dnsconf.read()
+            prof.DNS.PrimaryDNS = ''
+            prof.DNS.SecondaryDNS = ''
+            prof.DNS.TernaryDNS = ''
             if dnsconf.has_key('nameservers'):
                 prof.DNS.PrimaryDNS = dnsconf['nameservers'][0]
                 if len(dnsconf['nameservers']) > 1:
                     prof.DNS.SecondaryDNS = dnsconf['nameservers'][1]
-                else:
-                    prof.DNS.SecondaryDNS = ''
                 if len(dnsconf['nameservers']) > 2:
                     prof.DNS.TernaryDNS = dnsconf['nameservers'][2]
-                else:
-                    prof.DNS.TernaryDNS = ''
-
             sl = prof.DNS.createSearchList()
             if dnsconf.has_key('search'):
                 for ns in dnsconf['search']:
