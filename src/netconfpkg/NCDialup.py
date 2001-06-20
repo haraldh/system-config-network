d9e8e33fa2cb3851c4c101692ca5a9201c823498
harald <harald>
2001-06-20 13:13:22 +0000

    - made Device specific Dialup classes - rp3-config compatibility - Device config stuff moved in also

--- src/netconfpkg/NCDialup.py
+++ src/netconfpkg/NCDialup.py
@@ -5,9 +5,14 @@ from ConfSMB import *
 
 if not "/usr/lib/rhs/python" in sys.path:
     sys.path.append("/usr/lib/rhs/python")
-        
+
+import Conf
         
 class Dialup(Dialup_base):
+    def __init__(self, list = None, parent = None):
+        Dialup_base.__init__(self, list, parent)        
+
+class ModemDialup(Dialup):
     wvdict = { 'Login' : 'Username',
                'Password' : 'Password',
                'Prefix' : 'Dial Prefix',
@@ -16,7 +21,7 @@ class Dialup(Dialup_base):
                }                   
                         
     def __init__(self, list = None, parent = None):
-        Dialup_base.__init__(self, list, parent)        
+        Dialup.__init__(self, list, parent)        
 
     def loadIsdn(self, name):
         devdir = SYSCONFDEVICEDIR + name + '.d/'
@@ -29,7 +34,14 @@ class Dialup(Dialup_base):
             os.mkdir(devdir)
         
 
-    def loadModem(self, name):
+    def load(self, parentConf):
+        parent = self.getParent()
+        if parent:
+            modemsect = parent.DeviceId
+            
+        if parentConf.has_key('WVDIALSECT'):
+            modemsect = parentConf['WVDIALSECT']
+
         conf = ConfSMB(filename = '/etc/wvdial.conf')
         
         sectname = 'Dialer ' + name
@@ -62,13 +74,56 @@ class Dialup(Dialup_base):
                 if not self.InitStrings: self.createInitStrings()
                 #print confkey + " = " + value
                 self.InitStrings[self.InitStrings.addInitString()] = value
+                
+        #
+        # Workaround for backporting rp3-config stuff
+        #
+        if conf[sectname].has_key('Inherits'):
+            if conf[sectname]['Inherits'] != 'Dialer Defaults' and \
+               (len(conf[sectname]['Inherits']) > 5) and \
+               conf[sectname]['Inherits'][:5] == 'Modem':
+                if parent:
+                    parent.Device = conf[sectname]['Inherits']
+            elif conf[sectname]['Inherits'] == 'Dialer Defaults':
+                #print "Has Defaults!"
+                if conf.has_key('Dialer Defaults') and \
+                   conf['Dialer Defaults'].has_key('Modem') and \
+                   conf['Dialer Defaults'].has_key('Baud'):
+                    modemdev = conf['Dialer Defaults']['Modem']
+                    modembaud = conf['Dialer Defaults']['Baud']
+                    #print "Modem = " + modemdev
+                    #print "Baud = " + modembaud
+                    for sect in conf.keys():
+                        if (len(sect) <= 5) or (sect[:5] != 'Modem'):
+                            #print "Skipping " + sect
+                            continue
+                        if conf[sect].has_key('Modem') and \
+                           conf[sect].has_key('Baud') and \
+                           conf[sect]['Modem'] == modemdev and \
+                           conf[sect]['Baud'] == modembaud:
+                            print "Found " + sect
+                            if parent:                                
+                                parent.Device = sect
+                            break
         
-    def saveModem(self, name, device):
+    def save(self, parentConf):
         #devdir = SYSCONFDEVICEDIR + name + '.d/'
         #if not isdir(devdir):
         #    os.mkdir(devdir)
         #
+        parent = self.getParent()
+
+        if parent:                                
+            devname = parent.Device
+            parentConf['WVDIALSECT'] = devname
+            name = parent.DeviceId
+        else:
+            devname = '*'
+            name = "None"
         
+        if self.Login:
+            parentCconf['PAPNAME'] = self.Login
+                   
         conf = ConfSMB(filename = '/etc/wvdial.conf')
         sectname = 'Dialer ' + name
         
@@ -90,12 +145,30 @@ class Dialup(Dialup_base):
                 if conf[sectname].has_key(confkey):
                     del conf[sectname][confkey]
 
-        conf[sectname]['Inherits'] = device
+        conf[sectname]['Inherits'] = devname
                     
         conf.write()
 
+        print "device = " + devname
+        for secretfile in [ "/etc/ppp/pap-secrets", "/etc/ppp/chap-secrets" ]:
+            conf = Conf.Conf(secretfile, '#', ' \t', ' \t')
+            while conf.findnextcodeline():
+                vars = conf.getfields()
+                print vars
+                if vars and (len(vars) == 3) \
+                   and ((vars[0] == self.Login) \
+                        or (vars[0] == '"' + self.Login + '"')) \
+                        and (vars[1] == devname):
+                    print vars                    
+                    print conf.getline()
+                    print "login = " + self.Login
+                    pass
+                    
+                conf.nextline()
+
+
 if __name__ == '__main__':
     dl = Dialup()
-    dl.loadModem('phone2')
-    dl.Password = "mypassword"
-    dl.saveModem('phone2', 'ppp0')
+    dl.loadModem('test2')
+#    dl.Password = "mypassword"
+#    dl.saveModem('phone2', 'ppp0')
