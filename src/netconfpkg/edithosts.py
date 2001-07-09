201ea423b98b0492559c71c752df84b950d66c5d
pknirsch <pknirsch>
2001-07-09 13:38:35 +0000

    - Dialog code for Hosts add/edit operation.

---
+++ src/netconfpkg/edithosts.py
@@ -0,0 +1,130 @@
+#! /usr/bin/python
+
+## netconf - A network configuration tool
+## Copyright (C) 2001 Red Hat, Inc.
+## Copyright (C) 2001 Than Ngo <than@redhat.com>
+ 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+ 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+ 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import gtk
+import GDK
+import GTK
+import libglade
+import signal
+import os
+import GdkImlib
+import string
+import gettext
+import re
+
+from gtk import TRUE
+from gtk import FALSE
+from gtk import CTREE_LINES_DOTTED
+
+##
+## I18N
+##
+gettext.bindtextdomain("netconf", "/usr/share/locale")
+gettext.textdomain("netconf")
+_=gettext.gettext
+
+class editHostsDialog:
+    def __init__(self, host, xml_main = None):
+        self.host = host
+        self.xml_main = xml_main
+
+        glade_file = "edithosts.glade"
+
+        if not os.path.exists(glade_file):
+            glade_file = "netconfpkg/" + glade_file
+        if not os.path.exists(glade_file):
+            glade_file = "/usr/share/netconf/" + glade_file
+
+        self.xml = libglade.GladeXML(glade_file, None, domain="netconf")
+
+        self.xml.signal_autoconnect(
+            {
+            "on_addressEntry_insert_text" : (self.on_generic_entry_insert_text,
+                                             r"^[a-f|0-9\.]"),
+            "on_hostnameEntry_insert_text" : (self.on_generic_entry_insert_text,
+                                             r"^[\w|0-9\.]"),
+            "on_aliasesEntry_insert_text" : (self.on_generic_entry_insert_text,
+                                             r"^[\w| |0-9\.]"),
+            "on_okButton_clicked" : self.on_okButton_clicked,
+            "on_cancelButton_clicked" : self.on_cancelButton_clicked
+            })
+
+        self.dialog = self.xml.get_widget("Dialog")
+        self.dialog.connect("delete-event", self.on_Dialog_delete_event)
+        self.dialog.connect("hide", gtk.mainquit)
+        self.load_icon("network.xpm")
+        self.dialog.set_close(TRUE)
+        self.hydrate()
+
+    def load_icon(self, pixmap_file, widget = None):
+        if not os.path.exists(pixmap_file):
+            pixmap_file = "pixmaps/" + pixmap_file
+        if not os.path.exists(pixmap_file):
+            pixmap_file = "../pixmaps/" + pixmap_file
+        if not os.path.exists(pixmap_file):
+            pixmap_file = "/usr/share/netconf/" + pixmap_file
+        if not os.path.exists(pixmap_file):
+            return
+
+        pix, mask = gtk.create_pixmap_from_xpm(self.dialog, None, pixmap_file)
+        gtk.GtkPixmap(pix, mask)
+
+        if widget:
+            widget.set(pix, mask)
+        else:
+            self.dialog.set_icon(pix, mask)
+            
+    def on_Dialog_delete_event(self, *args):
+        self.host.rollback()
+
+    def on_okButton_clicked(self, button):
+        self.dehydrate()
+        self.host.commit()
+
+    def on_cancelButton_clicked(self, button):
+        self.host.rollback()
+
+    def on_generic_entry_insert_text(self, entry, partial_text, length,
+                                     pos, str):
+        text = partial_text[0:length]
+        if re.match(str, text):
+            return
+        entry.emit_stop_by_name('insert_text')
+
+    def hydrate(self):
+        if self.host.IP:
+            self.xml.get_widget('addressEntry').set_text(self.host.IP)
+        if self.host.Hostname:
+            self.xml.get_widget('hostnameEntry').set_text(self.host.Hostname)
+        if self.host.AliasList:
+            self.xml.get_widget('aliasesEntry').set_text(string.join(self.host.AliasList, ' '))
+
+    def dehydrate(self):
+        self.host.IP = self.xml.get_widget('addressEntry').get_text()
+        self.host.Hostname = self.xml.get_widget('hostnameEntry').get_text()
+        self.host.createAliasList()
+        for al in string.split(self.xml.get_widget('aliasesEntry').get_text(), ' '):
+            self.host.AliasList.append(al)
+
+# make ctrl-C work
+if __name__ == "__main__":
+    signal.signal (signal.SIGINT, signal.SIG_DFL)
+    window = editHostsDialog()
+    gtk.mainloop()
