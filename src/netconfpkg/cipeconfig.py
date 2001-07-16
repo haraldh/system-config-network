49a840d7018cdb6b8cf7b9912fe7388f5d31edda
harald <harald>
2001-07-16 15:27:39 +0000

    initial version

--- src/netconfpkg/cipeconfig.py
+++ src/netconfpkg/cipeconfig.py
@@ -28,7 +28,8 @@ import GdkImlib
 import string
 import gettext
 import string
-
+import commands
+import NCDeviceList
 import HardwareList
 from deviceconfig import deviceConfigDialog
 
@@ -48,6 +49,18 @@ class cipeConfigDialog(deviceConfigDialog):
         glade_file = "cipeconfig.glade"
         deviceConfigDialog.__init__(self, glade_file,
                                     device, xml_main, xml_basic)    
+        self.xml.signal_autoconnect(
+            {
+            "on_remotePeerAddressCB_toggled" : self.on_remotePeerAddressCB_toggled,
+            "on_generateKeyButton_clicked" : self.on_generateKeyButton_clicked,
+            "on_localPortEntry_changed" : self.updateRemoteOptions,
+            "on_ethernetDeviceEntry_changed" : self.updateRemoteOptions,
+            "on_cipeDeviceEntry_changed" : self.updateRemoteOptions,
+            "on_remoteVirtualAddressEntry_changed" : self.updateRemoteOptions,
+            "on_secretKeyEntry_changed" : self.updateRemoteOptions,
+            "on_remotePeerAddressEntry_changed" : self.updateRemoteOptions,
+            "on_localVirtualAddressEntry_changed" : self.updateRemoteOptions,
+            })
 
 
     def hydrate(self):
@@ -75,13 +88,108 @@ class cipeConfigDialog(deviceConfigDialog):
             widget.set_text(hwcurr)
         widget.set_position(0)
                 
-        if self.Device:
-            self.xml.get_widget("cipeDeviceEntry").set_text(self.Device)
+        if self.device.Device:
+            self.xml.get_widget("cipeDeviceEntry").set_text(self.device.Device)
 
-        if self.LocalPort:
-            self.xml.get_widget("").set_text(self.LocalPort)
+        if self.device.Cipe.LocalPort:
+            self.xml.get_widget("localPortEntry").set_text(str(self.device.Cipe.LocalPort))
 
-        #if self.RemotePeer
+        if self.device.Cipe.RemotePeerAddress:
+            vals = string.split(self.device.Cipe.RemotePeerAddress, ":")
+            addr = vals[0]
+            if len(vals) > 1:
+                port = vals[1]
+                self.xml.get_widget("remotePeerPortEntry").set_text(port)
+            self.xml.get_widget("remotePeerAddressEntry").set_text(addr)
+                
+        if self.device.Cipe.RemotePeerAddress == "0.0.0.0" \
+           or self.device.Cipe.RemotePeerAddress == "" \
+           or not self.device.Cipe.RemotePeerAddress:
+            self.xml.get_widget("remotePeerAddressEntry").set_sensitive(FALSE)
+            self.xml.get_widget("remotePeerPortEntry").set_sensitive(FALSE)
+            self.xml.get_widget("remotePeerAddressCB").set_active(TRUE)            
+
+            
+        if self.device.Cipe.RemoteVirtualAddress:
+            self.xml.get_widget("remoteVirtualAddressEntry").set_text(self.device.Cipe.RemoteVirtualAddress)
+        if self.device.IP: self.xml.get_widget("localVirtualAddressEntry").set_text(self.device.IP)
+        widget = self.xml.get_widget("secretKeyEntry")
+        if self.device.Cipe.SecretKey:
+            widget.set_text(self.device.Cipe.SecretKey)
+        widget.set_position(0)
+
+        self.updateRemoteOptions()
 
     def dehydrate(self):
-        self.Device = self.xml.get_widget("cipeDeviceEntry").get_text()
+        deviceConfigDialog.dehydrate(self)
+
+        hw = self.xml.get_widget("ethernetDeviceEntry").get_text()
+        fields = string.split(hw)
+        hw = fields[0]
+        self.device.TunnelDevice = hw
+
+        self.device.Device = self.xml.get_widget("cipeDeviceEntry").get_text()
+        self.device.Cipe.LocalPort = int(self.xml.get_widget("localPortEntry").get_text())
+        self.device.Cipe.RemoteVirtualAddress = self.xml.get_widget("remoteVirtualAddressEntry").get_text()
+        self.device.IP = self.xml.get_widget("localVirtualAddressEntry").get_text()
+        self.device.Cipe.SecretKey = self.xml.get_widget("secretKeyEntry").get_text()
+        if self.xml.get_widget("remotePeerAddressCB").get_active():
+            self.device.Cipe.RemotePeerAddress = "0.0.0.0"
+        else:
+            self.device.Cipe.RemotePeerAddress = self.xml.get_widget("remotePeerAddressEntry").get_text() + ":" + self.xml.get_widget("remotePeerPortEntry").get_text()
+
+    def on_protocolEditButton_clicked(self, *args):
+        self.device.IP = self.xml.get_widget("localVirtualAddressEntry").get_text()
+        deviceConfigDialog.on_protocolEditButton_clicked(self, args)
+        self.xml.get_widget("localVirtualAddressEntry").set_text(self.device.IP)
+        
+    def on_remotePeerAddressCB_toggled(self, *args):
+        if self.xml.get_widget("remotePeerAddressCB").get_active():
+            self.xml.get_widget("remotePeerAddressEntry").set_sensitive(FALSE)
+            self.xml.get_widget("remotePeerPortEntry").set_sensitive(FALSE)
+        else:
+            self.xml.get_widget("remotePeerAddressEntry").set_sensitive(TRUE)
+            self.xml.get_widget("remotePeerPortEntry").set_sensitive(TRUE)
+
+        self.updateRemoteOptions()
+
+    def on_generateKeyButton_clicked(self, *args):
+        key = commands.getoutput('(ps aux|md5sum; ps alx|md5sum) | tr -cd 0-9 2>/dev/null')
+        widget = self.xml.get_widget("secretKeyEntry")
+        if key:
+            widget.set_text(key)            
+        widget.set_position(0)
+        
+
+    def updateRemoteOptions(self, *args):
+
+        hw = self.xml.get_widget("ethernetDeviceEntry").get_text()
+        fields = string.split(hw)
+        hw = fields[0]
+
+        devlist = NCDeviceList.getDeviceList()
+        for dev in devlist:
+            if dev.Device == hw:
+                ip = dev.IP
+        
+        addr = self.xml.get_widget("remotePeerAddressEntry").get_text()
+        port = self.xml.get_widget("remotePeerPortEntry").get_text()
+        if not port or port == "":
+            port = "(own choice)"
+
+        localport = self.xml.get_widget("localPortEntry").get_text()
+        remotevirtualaddress = self.xml.get_widget("remoteVirtualAddressEntry").get_text()
+        localvirtualaddress = self.xml.get_widget("localVirtualAddressEntry").get_text()
+        secretkey = self.xml.get_widget("secretKeyEntry").get_text()
+                
+        mytxt = "Local Port: " + port + "\n"
+        mytxt = mytxt + "Remote Peer Address: " + str(ip) + ":" + str(localport) + "\n"
+        mytxt = mytxt + "Remote Virtual Address: " + str(localvirtualaddress) + "\n"
+        mytxt = mytxt + "Local Virtual Address: " + str(remotevirtualaddress) + "\n"
+        mytxt = mytxt + "Local IP Address: " + str(addr) + "\n"
+        mytxt = mytxt + "Secret Key: " + str(secretkey) + "\n"
+        widget = self.xml.get_widget("remoteConfigTxt")
+        if widget.get_length():
+            widget.delete_text(0, widget.get_length()-1)
+        widget.insert_defaults(mytxt)
+        widget.set_position(0)
