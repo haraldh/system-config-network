f710f44ba36c3c437bd17a5c58d3019c0d1fd770
harald <harald>
2001-07-19 16:03:26 +0000

    added copyright notice

--- src/netconf.py
+++ src/netconf.py
@@ -3,6 +3,8 @@
 ## netconf - A network configuration tool
 ## Copyright (C) 2001 Red Hat, Inc.
 ## Copyright (C) 2001 Than Ngo <than@redhat.com>
+## Copyright (C) 2001 Harald Hoyer <harald@redhat.com>
+## Copyright (C) 2001 Philipp Knirsch <pknirsch@redhat.com>
 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
