bc33805beae5dc1c056dc1c2d134b18c8c5c81ed
harald <harald>
2002-06-04 13:18:26 +0000

    cast to int()

---
+++ src/netconfpkg/NCCard.py
@@ -0,0 +1,32 @@
+## Copyright (C) 2001, 2002 Red Hat, Inc.
+## Copyright (C) 2001, 2002 Than Ngo <than@redhat.com>
+## Copyright (C) 2001, 2002 Harald Hoyer <harald@redhat.com>
+## Copyright (C) 2001, 2002 Philipp Knirsch <pknirsch@redhat.com>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+from netconfpkg import Card_base
+from NC_functions import *
+
+class Card(Card_base):
+    def __init__(self, list = None, parent = None):
+        Card_base.__init__(self, list, parent)
+
+    def setDMA0(self, value):
+        Card_base.setDMA0(int(value))
+
+    def setDMA1(self, value):
+        Card_base.setDMA1(int(value))
