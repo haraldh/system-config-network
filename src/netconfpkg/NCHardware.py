a9bb74f5dc4e522f9d795bbc43f71b248a164ead
harald <harald>
2002-12-13 16:08:28 +0000

    Hardware Factory

---
+++ src/netconfpkg/NCHardware.py
@@ -0,0 +1,33 @@
+## Copyright (C) 2001, 2002 Red Hat, Inc.
+## Copyright (C) 2001, 2002 Harald Hoyer <harald@redhat.com>
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from NC_functions import *
+from netconfpkg import Hardware_base
+
+class Hardware(Hardware_base):
+    def __init__(self, list = None, parent = None):
+        Hardware_base.__init__(self, list, parent)        
+
+    def getDialog(self):
+        raise "getDialog not Implemented"
+
+    def getWizard(self):
+        raise "getWizard not Implemented"
+
+    def isType(self, device):
+        raise "isType not Implemented"
+
