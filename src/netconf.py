9dbbd6108744f9be7348143416c5f7c984663ea8
harald <harald>
2003-12-16 11:19:46 +0000

    added utf-8 encoding comment removed date and version

--- src/netconf.py
+++ src/netconf.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 ## netconf - A network configuration tool
 ## Copyright (C) 2001-2003 Red Hat, Inc.
 ## Copyright (C) 2001, 2002 Than Ngo <than@redhat.com>
 ## Copyright (C) 2001-2003 Harald Hoyer <harald@redhat.com>
 ## Copyright (C) 2001, 2002 Philipp Knirsch <pknirsch@redhat.com>
-## Copyright (C) 2001, 2002 Trond Eivind Glomsrød <teg@redhat.com>
+## Copyright (C) 2001, 2002 Trond Eivind GlomsrÃ¸d <teg@redhat.com>
 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -268,6 +269,5 @@ if __name__ == '__main__':
         main()
         
     sys.exit(0)
+
 __author__ = "Harald Hoyer <harald@redhat.com>"
-__date__ = "$Date: 2003/12/02 10:28:14 $"
-__version__ = "$Revision: 1.202 $"
