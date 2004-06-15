fd214bb51d9c5ce5eecfe75021d40f7a4264f68b
harald <harald>
2004-06-15 13:59:06 +0000

    use UnitTest and coverage for the start of a testsuite

--- src/netconfpkg/tests/test-Conf.py
+++ src/netconfpkg/tests/test-Conf.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
+
+import unittest
 import sys
 import string
 import os
-
-if __name__ != '__main__':
-    print "Usage: %s" % sys.argv[0]
-    sys.exit(-1)
 
 def writeConf(filename, str):
     try: os.unlink(filename)
@@ -22,63 +20,61 @@ def expectConf(filename, str):
     lines = file.readlines()
     lines = string.join(lines, '')
 
-    if lines != str:
-        return 1
+    return (lines == str)
 
-from rhpl import Conf
-
-#
-# test1
-#
-filename = "test1.out"
-str = """
+class TestConf(unittest.TestCase):
+    filename = "test.out"
+    def tearDown(self):
+        try: 
+            os.unlink(self.filename)
+        except: pass            
+        
+    def testReadWrite(self):
+        """Conf class: basic reading and writing"""
+        str = """
 # testConfig file
 a\tb
 # comment
 c\td
 # comment2
 """
-# read
-writeConf(filename, str)
-conf = Conf.Conf(filename)
-os.unlink(filename)
-# write
-conf.write()
-del conf
-# check
-if expectConf(filename, str):
-    writeConf(filename + '.orig', str)
-    print "Test1 failed!!!!"
-    os.system("diff -u " + filename + " " + filename + ".orig")
-    sys.exit(10)
-# cleanup
-os.unlink(filename)
+        # read
+        writeConf(self.filename, str)
+        conf = Conf.Conf(self.filename)
+        os.unlink(self.filename)
+        # write
+        conf.write()
+        del conf
+        # check
+        if not expectConf(self.filename, str):
+            writeConf(self.filename + '.orig', str)
+            os.system("diff -u " + self.filename + " " + self.filename + ".orig")
+            os.unlink(self.filename + '.orig')
+            self.fail("Test1 failed!!!!")
 
 
-#
-# test2
-#
-filename = "test2.out"
-str = """
+    def testModify(self):
+        """Conf class: reading, modify and writing"""
+        str = """
 # testConfig file
 a=b
 # comment
 c=d
 # comment2
 """
-# read
-writeConf(filename, str)
-conf = Conf.ConfShellVar(filename)
-os.unlink(filename)
-# modify
-conf["a"] = "e"
-conf["c"] = "f"
-conf["g"] = "h"
-# write
-conf.write()
-del conf
-# check
-str = """
+        # read
+        writeConf(self.filename, str)
+        conf = Conf.ConfShellVar(self.filename)
+        os.unlink(self.filename)
+        # modify
+        conf["a"] = "e"
+        conf["c"] = "f"
+        conf["g"] = "h"
+        # write
+        conf.write()
+        del conf
+        # check
+        str = """
 # testConfig file
 a=e
 # comment
@@ -86,19 +82,19 @@ c=f
 # comment2
 g=h
 """
-if expectConf(filename, str):
-    writeConf(filename + '.orig', str)
-    print "Test2 failed!!!!"
-    os.system("diff -u " + filename + " " + filename + ".orig")
-    sys.exit(10)
-# cleanup
-os.unlink(filename)
+        if not expectConf(self.filename, str):
+            writeConf(self.filename + '.orig', str)
+            os.system("diff -u " + self.filename + " " + self.filename + ".orig")
+            os.unlink(self.filename + '.orig')
+            self.fail("Test2 failed!!!!")
 
-#
-# test3
-#
-filename = "test3.out"
-str = """# Modules test file
+
+    def testConfModules(self):
+        """ConfModules class: basic reading and writing"""
+        #
+        # test3
+        #
+        str = """# Modules test file
 alias parport_lowlevel parport_pc
 #alias eth0 3c59x
 alias eth3 3c59x
@@ -111,27 +107,26 @@ post-install sound-slot-0 /bin/aumix-minimal -f /etc/.aumixrc -L >/dev/null 2>&1
 pre-remove sound-slot-0 /bin/aumix-minimal -f /etc/.aumixrc -S >/dev/null 2>&1 || :alias usb-controller usb-uhci
 post-install sound-slot-1 /bin/aumix-minimal -f /etc/.aumixrc -L >/dev/null 2>&1 || :
 """
-# read
-writeConf(filename, str)
-conf = Conf.ConfModules(filename)
-os.unlink(filename)
-# write
-conf.write()
-del conf
-# check
-if expectConf(filename, str):
-    writeConf(filename + '.orig', str)
-    print "Test3 failed!!!!"
-    os.system("diff -u " + filename + " " + filename + ".orig")
-    sys.exit(10)
-# cleanup
-os.unlink(filename)
+        # read
+        writeConf(self.filename, str)
+        conf = Conf.ConfModules(self.filename)
+        os.unlink(self.filename)
+        # write
+        conf.write()
+        del conf
+        # check
+        if not expectConf(self.filename, str):
+            writeConf(self.filename + '.orig', str)
+            os.system("diff -u " + self.filename + " " + self.filename + ".orig")
+            os.unlink(self.filename + '.orig')
+            self.fail("Test3 failed!!!!")
 
-#
-# test4
-#
-filename = "test4.out"
-str = """# Modules test file
+    def testConfModulesModify(self):
+        """ConfModules class: reading, modify and writing"""
+        #
+        # test4
+        #
+        str = """# Modules test file
 alias parport_lowlevel parport_pc
 #alias eth0 3c59x
 alias eth3 3c59x
@@ -147,18 +142,18 @@ options parport_pc io=0x378 irq=7
 options nsc-ircc io=0x02f8 dongle_id=0x09 irq=3 dma=0
 options 3c59x debug=2
 """
-# read
-writeConf(filename, str)
-conf = Conf.ConfModules(filename)
-os.unlink(filename)
-# modify
-conf["eth3"]["alias"] = "3c59xaaa"
-conf["eth0"] = { "alias" : "3c59x" }
-# write
-conf.write()
-del conf
-# check
-str = """# Modules test file
+        # read
+        writeConf(self.filename, str)
+        conf = Conf.ConfModules(self.filename)
+        os.unlink(self.filename)
+        # modify
+        conf["eth3"]["alias"] = "3c59xaaa"
+        conf["eth0"] = { "alias" : "3c59x" }
+        # write
+        conf.write()
+        del conf
+        # check
+        str = """# Modules test file
 alias parport_lowlevel parport_pc
 #alias eth0 3c59x
 alias eth3 3c59xaaa
@@ -175,16 +170,29 @@ options nsc-ircc io=0x02f8 dongle_id=0x09 irq=3 dma=0
 options 3c59x debug=2 
 alias eth0 3c59x
 """
-if expectConf(filename, str):
-    writeConf(filename + '.orig', str)
-    print "Test4 failed!!!!"
-    os.system("diff -u " + filename + " " + filename + ".orig")
-    sys.exit(10)
-# cleanup
-os.unlink(filename)
+        if not expectConf(self.filename, str):
+            writeConf(self.filename + '.orig', str)
+            os.system("diff -u " + self.filename + " " + self.filename + ".orig")
+            os.unlink(self.filename + '.orig')
+            self.fail("Test4 failed!!!!")
 
+def suite():
+    suite = unittest.TestSuite()
+    suite = unittest.makeSuite(TestConf,'test')
+    return suite
 
-sys.exit(0)
+if __name__ == "__main__":
+    import coverage
+    coverage.erase()
+    coverage.start()
+    from rhpl import Conf
+    testRunner = unittest.TextTestRunner(verbosity=2)
+    result = testRunner.run(suite())
+    coverage.stop()    
+    m = sys.modules.values()
+    coverage.the_coverage.report(Conf, show_missing=0 )    
+    sys.exit(not result.wasSuccessful())
+    
 __author__ = "Harald Hoyer <harald@redhat.com>"
-__date__ = "$Date: 2003/07/08 09:45:48 $"
-__version__ = "$Revision: 1.9 $"
+__date__ = "$Date: 2004/06/15 13:59:06 $"
+__version__ = "$Revision: 1.10 $"
