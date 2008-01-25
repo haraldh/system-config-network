58c6a0fb4ef5bc191d51bae343508ac3d2a50a82
Jiri Moskovcak <jmoskovc@dhcp-lab-208.englab.brq.redhat.com>
2008-01-25 16:32:55 +0100

    Minor fix in hosts parser - now it accepts trailing dot.

--- src/netconfpkg/NCHost.py
+++ src/netconfpkg/NCHost.py
@@ -7,6 +7,9 @@ def testHostname(hostname):
     # hostname: names separated by '.' every name must be max 63 chars in length and the hostname max length is 255 chars
     if (len(hostname) - hostname.count('.')) < 256:
         names = hostname.split('.')
+        # hostname with trailing dot
+        if not names[-1]:
+            names.pop()
         pattern = re.compile('([a-zA-Z]|[0-9])+(-[a-zA-Z]|-[0-9]|[a-zA-Z]|[0-9])*$')
         for name in names:
            if len(name) < 64:
