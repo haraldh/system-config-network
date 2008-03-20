21e4f2f859bc7f1435edc7c7469764cd62f1e8a8
Harald Hoyer <harald@redhat.com>
2008-03-20 10:32:33 +0100

    moved all git stuff in Makefile.inc
    merged Devel-2.0 autofoo
    set version to 1.5.99

--- autogen.sh
+++ autogen.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-THEDIR=`pwd`
-cd $srcdir
+cp -p /usr/share/aclocal/nls.m4 m4/nls.m4
+cp -p /usr/share/aclocal/po.m4 m4/po.m4
+cp -p /usr/share/aclocal/progtest.m4 m4/progtest.m4
+cp -p /usr/share/gettext/po/remove-potcdate.sin po/remove-potcdate.sin
 
 DIE=0
 
-glib-gettextize --copy --force
 intltoolize --copy -f --automake
 aclocal -I m4
 
 autoreconf -i -m -f -I m4
-cd $THEDIR
+./configure --enable-maintainer-mode "$@"
 
-$srcdir/configure --enable-maintainer-mode "$@"
-
-echo 
+echo
 echo "Now type 'make' to compile system-config-network."
+
