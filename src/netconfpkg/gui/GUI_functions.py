2852d8d5a729047674251ad6e541fe4f6afbac60
Harald Hoyer <harald@redhat.com>
2008-03-29 15:29:01 +0100

    removed duplicated function, added Exception base class to exception

--- src/netconfpkg/gui/GUI_functions.py
+++ src/netconfpkg/gui/GUI_functions.py
@@ -229,36 +229,6 @@ def gui_longinfo_dialog ( message, long_message, parent_dialog=None,
     dialog.destroy()
     return ret
 
-def gui_error_dialog ( message, parent_dialog=None,
-                      message_type=gtk.MESSAGE_ERROR,
-                      widget=None, page=0, broken_widget=None ):
-
-    dialog = gtk.MessageDialog( parent_dialog,
-                               gtk.DIALOG_MODAL|gtk.DIALOG_DESTROY_WITH_PARENT,
-                               message_type, gtk.BUTTONS_OK,
-                               message )
-
-    if widget != None:
-        if isinstance ( widget, gtk.CList ):
-            widget.select_row ( page, 0 )
-        elif isinstance ( widget, gtk.Notebook ):
-            widget.set_current_page ( page )
-    if broken_widget != None:
-        broken_widget.grab_focus ()
-        if isinstance ( broken_widget, gtk.Entry ):
-            broken_widget.select_region ( 0, -1 )
-
-    if parent_dialog:
-        dialog.set_position ( gtk.WIN_POS_CENTER_ON_PARENT )
-        dialog.set_transient_for( parent_dialog )
-    else:
-        dialog.set_position ( gtk.WIN_POS_CENTER )
-
-    ret = dialog.run ()
-    dialog.destroy()
-    return ret
-
-
 def gui_yesnocancel_dialog ( message, parent_dialog,
                             message_type=gtk.MESSAGE_QUESTION,
                             widget=None, page=0, broken_widget=None ):
@@ -444,7 +414,8 @@ def gui_run_dialog( command, argv, searchPath = 0,
     import string
     global __cancelPressed
     global __dialogClosed
-    class CancelException: pass
+    class CancelException(Exception):
+        "raise if canceled"
 
     __cancelPressed = 0
     __dialogClosed = 0
