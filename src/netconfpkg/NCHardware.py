86aed1817f7668fe88ac6319a46d1e04de75dcaf
Harald Hoyer <harald@redhat.com>
2008-04-14 16:19:30 +0200

    import log

--- src/netconfpkg/NCHardware.py
+++ src/netconfpkg/NCHardware.py
@@ -14,7 +14,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-from netconfpkg.NC_functions import _
+from netconfpkg.NC_functions import _, log
 from netconfpkg.gdt import (Gdtstruct, gdtstruct_properties, Gdtstr,
                             Gdtint)
 
